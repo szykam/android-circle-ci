c4d09f3ef452737fe85f53e1c6c329e014bdc9af
Szymon Kamycki <szymon.kamycki@gmail.com>
2018-07-09 20:38:57 +0200

    Initial commit.

---
+++ app/src/androidTest/java/pl/skamycki/circleci/MainActivityTest.kt
@@ -0,0 +1,22 @@
+package pl.skamycki.circleci
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun activityLaunched() {
+        onView(withText("Hello World!"))
+                .check(matches(isDisplayed()))
+    }
+}
