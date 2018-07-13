ad4ce2fb61509d88adf706df047c55ac5b31d156
Szymon Kamycki <szymon.kamycki@gmail.com>
2018-07-13 13:37:24 +0200

    Added forced crash.

--- app/src/main/java/pl/skamycki/circleci/MainActivity.kt
+++ app/src/main/java/pl/skamycki/circleci/MainActivity.kt
@@ -2,11 +2,13 @@ package pl.skamycki.circleci
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        crash_btn.setOnClickListener { throw RuntimeException("Forced crash") }
     }
 }
