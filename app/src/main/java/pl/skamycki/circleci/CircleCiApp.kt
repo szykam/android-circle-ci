1dbf272d2f2a734c3c799792e316d063fd138f23
Szymon Kamycki <szymon.kamycki@gmail.com>
2018-07-13 14:45:49 +0200

    Added `Answers` for logging purposes. - enabled progurad for `release` build

--- app/src/main/java/pl/skamycki/circleci/CircleCiApp.kt
+++ app/src/main/java/pl/skamycki/circleci/CircleCiApp.kt
@@ -2,12 +2,16 @@ package pl.skamycki.circleci
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
 import io.fabric.sdk.android.Fabric
 
 class CircleCiApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Fabric.with(this, Crashlytics())
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, Crashlytics())
+            Fabric.with(this, Answers())
+        }
     }
 }
--- app/src/main/java/pl/skamycki/circleci/MainActivity.kt
+++ app/src/main/java/pl/skamycki/circleci/MainActivity.kt
@@ -2,6 +2,8 @@ package pl.skamycki.circleci
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.answers.LoginEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         crash_btn.setOnClickListener { throw RuntimeException("Forced crash") }
+        login_btn.setOnClickListener { Answers.getInstance().logLogin(LoginEvent()) }
     }
 }
