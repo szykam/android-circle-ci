a8fb9f57d19956aa24788c803c50efd5d6e93cb3
Szymon Kamycki <szymon.kamycki@gmail.com>
2018-07-15 13:48:47 +0200

    Added failing test for production.

---
+++ app/src/testProd/java/pl/skamycki/circleci/ExampleUnitTestProd.kt
@@ -0,0 +1,18 @@
+package pl.skamycki.circleci
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTestProd {
+
+    @Test
+    fun failingTestForProd() {
+        fail("Forced failing production test")
+    }
+}
